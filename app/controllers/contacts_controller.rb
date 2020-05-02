5ac707c5a6a7547594a425315562bbe5d9bf5d14
Amit Maji <amitmaji222@gmail.com>
2020-05-02 03:43:02 +0000

    Added code comments

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,24 +1,42 @@
 class ContactsController < ApplicationController
+  
+  # GET request to /contact-us
+  # Show new contact form
+  
   def new
     
     @contact = Contact.new
       
   end
+  
+  # POST request /cantacts
+  
   def create
+    # Mass assignment of form fields into Contact object
     @contact = Contact.new(contact_params)
+    # Save the Contact object to the database
     if @contact.save
+      # Store form fields via parameters, into variables
       name = params[:contact][:name]
       email = params[:contact][:email]
       comments = params[:contact][:comments]
+      # Plug variables into Cantact Mailer 
+      # email method and send email
       ContactMailer.contact_email(name, email, comments).deliver
+      #Store succes message in flash hash
+      # and redirect to the new action
       flash[:success] = "Message Sent"
       redirect_to new_contact_path
     else
+      # If Contact object doesn't save
+      # and redirect to the new action
       flash[:danger] = @contact.errors.full_messages.join(", ")
       redirect_to new_contact_path
     end  
   end
   private
+    # To collect data from form, we need to use 
+    # strong parameters and whitelist the form fields 
     def contact_params
       params.require(:contact).permit(:name, :email, :comments)
       
